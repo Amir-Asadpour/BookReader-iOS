40edbb71fcb9d0886a3057d02d8e9c922c8ba1b5
Amir Asadpour <asadpour.ah@gmail.com>
2024-09-21 11:22:10 +0200

    Initial Commit

---
+++ BookReader/BookReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderApp.swift
+//  BookReader
+//
+//  Created by Amir Asadpour on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BookReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookReaderTests/BookReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderTests.swift
+//  BookReaderTests
+//
+//  Created by Amir Asadpour on 9/21/24.
+//
+
+import Testing
+@testable import BookReader
+
+struct BookReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
